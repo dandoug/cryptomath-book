99cb957fed8e6cbc337f404b5b105c587d8a82b0
Dan Douglas <dandoug@gmail.com>
2025-07-11 14:50:22 -0700

    add file with top ngrams defined as dict

---
+++ src/ngrams_data/top_ngrams.py
@@ -0,0 +1,38 @@
+"""
+   Top ngrams of various length for use in cryptoanalysis
+
+   source: http://practicalcryptography.com/cryptanalysis/letter-frequencies-various-languages/english-letter-frequencies/
+"""
+
+TOP_ENGLISH_QUADGRAMS = {
+    "TION": 0.31,
+    "NTHE": 0.27,
+    "THER": 0.24,
+    "THAT": 0.21,
+    "OFTH": 0.19,
+    "FTHE": 0.19,
+    "THES": 0.18,
+    "WITH": 0.18,
+    "INTH": 0.17,
+    "ATIO": 0.17,
+    "OTHE": 0.16,
+    "TTHE": 0.16,
+    "DTHE": 0.15,
+    "INGT": 0.15,
+    "ETHE": 0.15,
+    "SAND": 0.14,
+    "STHE": 0.14,
+    "HERE": 0.13,
+    "THEC": 0.13,
+    "MENT": 0.12,
+    "THEM": 0.12,
+    "RTHE": 0.12,
+    "THEP": 0.11,
+    "FROM": 0.10,
+    "THIS": 0.10,
+    "TING": 0.10,
+    "THEI": 0.10,
+    "NGTH": 0.10,
+    "IONS": 0.10,
+    "ANDT": 0.10
+}
